c0d25b89a5269a671e39422319f87d907f7ed8e0
[Stephanie Renata s3726196] <s3726196@student.rmit.edu.au>
2021-01-28 15:07:43 +1100

    added elements to breath page

--- src/components/b/Breath.js
+++ src/components/b/Breath.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import NavBar from '../NavBar';
+import './Breath.css';
 
 const Breath = () =>{
   return(
     <div className = "Breath">
       <NavBar />
         <h1>Breath</h1>
+
+        <div className = "outer">
+          <div className = "inhale"><label className = "inhale-text">inhale</label></div>
+          <div className = "exhale"><label className = "exhale-text">exhale</label></div>
+        </div>
     </div>
   );    
 };
--- src/components/m/Meditation.js
+++ src/components/m/Meditation.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import NavBar from '../NavBar';
+import './Meditation.css'
+import ReactPlayer from 'react-player';
 
-const Meditation = () =>{
-  return(
-    <div className = "Meditation">
+const Meditation = () => {
+  return (
+    <div className="Meditation">
       <NavBar />
-        <h1>Meditation</h1>
+      <h1>Meditation</h1>
+
+      <li className = "video-list">
+        <ul>
+          <ReactPlayer url= "https://www.youtube.com/watch?v=gUqLVa34S3c" />
+        </ul>
+        <ul>
+          <ReactPlayer url= "https://www.youtube.com/watch?v=TWrnP8-s_P8" />
+        </ul>
+        <ul>
+          <ReactPlayer url= "https://www.youtube.com/watch?v=ykDPtWdxOxs" />
+        </ul>
+        <ul>
+          <ReactPlayer url= "https://www.youtube.com/watch?v=8NB3ihqPQGw" />
+        </ul>
+
+        <ul>
+          <ReactPlayer url= "https://www.youtube.com/watch?v=kTUGo6ZduPg" />
+        </ul>
+
+        <ul>
+          <ReactPlayer url= "https://www.youtube.com/watch?v=buPuB4Sa0zU" />
+        </ul>
+        <ul>
+          <ReactPlayer url= "" />
+        </ul>
+      </li>
+      
+      
     </div>
-  );    
+  );
 };
 
 export default Meditation;
